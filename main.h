2cca6b3ffebebde402dd9edd09f75591721ba372
ACHIENG SHARON MASIGA <134079083+Isachi25@users.noreply.github.com>
2023-08-23 03:38:37 +0300

    main.h

--- main.h
+++ main.h
@@ -1,11 +1,11 @@
-#ifndef _PRINTF_H
-#define _PRINTF_H
+#ifndef _MAIN_H
+#define _MAIN_H
 
-#include <stdarg.h>
-#include <stdio.h>
 #include <unistd.h>
 #include <limits.h>
 #include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
 
 #define OUTPUT_BUF_SIZE 1024
 #define BUF_FLUSH -1
@@ -90,16 +90,16 @@ int get_flag(char *s, params_t *params);
 int get_modifier(char *s, params_t *params);
 char *get_width(char *s, params_t *params, va_list ap);
 
+/* simple_printers.c module */
+int print_from_to(char *start, char *stop, char *except);
+int print_rev(va_list ap, params_t *params);
+int print_rot13(va_list ap, params_t *params);
+
 /* convert_number.c module */
 int print_hex(va_list ap, params_t *params);
 int print_HEX(va_list ap, params_t *params);
 int print_binary(va_list ap, params_t *params);
 int print_octal(va_list ap, params_t *params);
-
-/* simple_printers.c module */
-int print_from_to(char *start, char *stop, char *except);
-int print_rev(va_list ap, params_t *params);
-int print_rot13(va_list ap, params_t *params);
 
 /* print_number.c module */
 int _isdigit(int c);
